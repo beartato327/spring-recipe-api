7972952b93c184bce881dd445379148a016e63c2
Roberto Capurro <recapurro327@gmail.com>
2024-02-28 14:13:12 -0600

    init repo

---
+++ src/main/java/com/recap/springrecipeapi/SpringRecipeApiApplication.java
@@ -0,0 +1,13 @@
+package com.recap.springrecipeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecipeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recap/springrecipeapi/SpringRecipeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recap.springrecipeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
